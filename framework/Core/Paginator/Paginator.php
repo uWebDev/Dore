9f5ec565e9b136af22710035bb5b15746e0f2009
Denis <uWebDev@users.noreply.github.com>
2016-05-06 14:30:28 +0400

    Update Paginator.php

--- framework/Core/Paginator/Paginator.php
+++ framework/Core/Paginator/Paginator.php
@@ -2,47 +2,6 @@
 
 namespace Dore\Core\Paginator;
 
-/*
-  Описание методов:
-  1. setCurrentPage - для установления номера текущей страницы
-  2. setRecordsCount - для установления общего количества записей
-  3. setMaxPageCount - для установления максимального количества страниц пагинатора
-  4. setPerPageLimit - для установления количества выводимых записей на одну страницу
-  5. getPages - для получения массива данных о страницах
-
-  Пример использования:
-  $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-
-  $paginator = new Paginator();
-  $pages = $paginator->setCurrentPage($page)
-  ->setRecordsCount(200)
-  ->setPerPageLimit(10)
-  ->setMaxPageCount(5)
-  ->getPages();
-
-  Если к примеру Вы переключитесь на вторую страничку, то в массиве появится дополнительный ключ 'prev' и значение(число) предыдущей странички.
-  Так же если нет страничек(высчитывается на основе переданных данных) "вперед", то ключа next в массиве так же не будет.
-
-  Ну и наконец пример вывода.
-
-  if(isset($pages['prev'])){
-  echo '<a href="?page=1"><<</a>
-  <a href="?page='.$pages['prev'].'"><</a>
-  ';
-  }
-
-  foreach($pages['pages'] as $page){
-  $currentPageClass = $page == $pages['current'] ? 'class="cur_page"' : '';
-  echo '<a href="?page='.$page.'" '.$currentPageClass .' >'.$page.'</a> ';
-  }
-
-  if(isset($pages['next'])){
-  echo '<a href="?page='.$pages['next'].'">></a>
-  <a href="?page='.$pages['last'].'">>></a>
-  ';
-  }
- */
-
 class Paginator
 {
 
